f91dcc67ab99ea089834f72dd5db0a4afada6285
MK-Khan123 <mehnazkhan231@gmail.com>
2022-07-19 02:21:31 +0600

    Fixed the color on CovidUpdates component for dark mode

--- src/components/Home/CovidUpdates/CovidUpdates.js
+++ src/components/Home/CovidUpdates/CovidUpdates.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,16 +7,43 @@ import Typography from '@mui/material/Typography';
 import vaccinationImage from '../../../images/covid-updates/svg_sample_2.svg';
 
 const CovidUpdates = () => {
+
+    const theme = useTheme();
+
     return (
         <Card sx={{ minWidth: 200, marginBottom: '1rem', borderRadius: '8px', mt: 4 }}>
             <CardContent sx={{ padding: 0, ':last-child': { padding: 0 } }}>
                 <Box
                     sx={{ display: { md: 'flex', alignItems: 'center', justifyContent: 'space-between' } }}
                 >
-                    <Typography variant='h5' fontWeight='600' fontSize='1.25rem' gutterBottom component='div' p={2}>Covid-19 Updates</Typography>
+                    <Typography
+                        variant='h5'
+                        fontWeight='600'
+                        fontSize='1.25rem'
+                        gutterBottom
+                        component='div'
+                        p={2}
+                    >
+                        Covid-19 Updates
+                    </Typography>
                     <Box sx={{ p: { md: 2 } }}>
-                        <Typography variant='body1' fontWeight='500' fontSize='1rem' pl={2}>10 September 2022</Typography>
-                        <Typography variant='body1' fontSize='1rem' color='#8A8686' gutterBottom pl={2}>Thursday 10:00 AM</Typography>
+                        <Typography
+                            variant='body1'
+                            fontWeight='500'
+                            fontSize='1rem'
+                            pl={2}
+                        >
+                            10 September 2022
+                        </Typography>
+                        <Typography
+                            variant='body1'
+                            fontSize='1rem'
+                            color={theme.palette.mode === 'dark' ? '' : '#8A8686'}
+                            gutterBottom
+                            pl={2}
+                        >
+                            Thursday 10:00 AM
+                        </Typography>
                     </Box>
                 </Box>
 
@@ -43,12 +70,44 @@ const CovidUpdates = () => {
                                 }}
                             >
                                 <>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='0.875rem' gutterBottom>Infection Number</Typography>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='2.4rem' gutterBottom>500</Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='0.875rem'
+                                        gutterBottom
+                                    >
+                                        Infection Number
+                                    </Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='2.4rem'
+                                        gutterBottom
+                                    >
+                                        500
+                                    </Typography>
                                 </>
                                 <>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='0.875rem' gutterBottom>Infection Rate</Typography>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='2.4rem' gutterBottom>10%</Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='0.875rem'
+                                        gutterBottom
+                                    >
+                                        Infection Rate
+                                    </Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='2.4rem'
+                                        gutterBottom
+                                    >
+                                        10%
+                                    </Typography>
                                 </>
                             </Box>
                             <Box
@@ -60,12 +119,44 @@ const CovidUpdates = () => {
                                 }}
                             >
                                 <>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='0.875rem' gutterBottom>Total Infection</Typography>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='2.4rem' gutterBottom>500,000</Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='0.875rem'
+                                        gutterBottom
+                                    >
+                                        Total Infection
+                                    </Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='2.4rem'
+                                        gutterBottom
+                                    >
+                                        500,000
+                                    </Typography>
                                 </>
                                 <>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='0.875rem' gutterBottom>Number of Death</Typography>
-                                    <Typography textAlign='center' variant='body1' color='#FFF' fontSize='2.4rem' gutterBottom>20</Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='0.875rem'
+                                        gutterBottom
+                                    >
+                                        Number of Death
+                                    </Typography>
+                                    <Typography
+                                        textAlign='center'
+                                        variant='body1'
+                                        color='#FFF'
+                                        fontSize='2.4rem'
+                                        gutterBottom
+                                    >
+                                        20
+                                    </Typography>
                                 </>
                             </Box>
                         </Box>
